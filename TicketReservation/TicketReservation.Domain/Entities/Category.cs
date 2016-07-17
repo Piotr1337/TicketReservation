65e323d6bbb476c91a5f6366d4f06b9b174cba47
azanwrc <azanwrc@gmail.com>
2016-07-17 19:44:58 +0200

    Ikonki i takie tam

--- TicketReservation/TicketReservation.Domain/Entities/Category.cs
+++ TicketReservation/TicketReservation.Domain/Entities/Category.cs
@@ -20,6 +20,7 @@ namespace TicketReservation.Domain.Entities
         public int EventCategoryID { get; set; }
         public string EventCategoryDescription { get; set; }
         public string EventCategoryName { get; set; }
+        public string Icon { get; set; }
         public ICollection<SubCategory> SubCategories { get; set; }
     }
 }
---
+++ TicketReservation/TicketReservation/scripts/navBarHover.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    function bindNavbar() {
+        if ($(window).width() > 768) {
+            $('.navbar-collapse .dropdown').on('mouseover', function () {
+                $('.dropdown-toggle', this).next('.dropdown-menu').show();
+            }).on('mouseout', function () {
+                $('.dropdown-toggle', this).next('.dropdown-menu').hide();
+            });
+        }
+        else {
+            $('.navbar-collapse .dropdown').off('mouseover').off('mouseout');
+        }
+    }
+
+    $(window).resize(function () {
+        bindNavbar();
+    });
+
+    bindNavbar();
+});
