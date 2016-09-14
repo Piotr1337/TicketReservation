563bc044abf0853091d18b12f7cc4d81b077f5d1
azanwrc <azanwrc@gmail.com>
2016-09-15 00:04:02 +0200

    Lazy image load

--- TicketReservation/TicketReservation/scripts/myScripts.js
+++ TicketReservation/TicketReservation/scripts/myScripts.js
@@ -7,3 +7,21 @@
         $('.validate').remove();
     }
 });
+
+; (function () {
+    // Initialize
+    var bLazy = new Blazy({
+
+        selector: 'img',
+        offset: 100,
+        success: function (element) {
+            setTimeout(function () {
+                // We want to remove the loader gif now.
+                // First we find the parent container
+                // then we remove the "loading" class which holds the loader image
+                var parent = element.parentNode;
+                parent.className = parent.className.replace(/\bloading\b/, '');
+            }, 200);
+        }
+    });
+})();
